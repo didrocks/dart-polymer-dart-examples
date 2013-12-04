bf6ed03df63c9d363af44ce95da04a92c251e3d4
Didier Roche <didrocks@ubuntu.com>
2013-12-04 14:16:55 +0100

    picky: the template is already Hello {{data['msg']}}. Avoid repetition.

--- web/bind_to_a_map/my_example.dart
+++ web/bind_to_a_map/my_example.dart
@@ -2,7 +2,7 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('my-example')
 class MyExample extends PolymerElement {
-  @observable Map data = toObservable({'msg': 'Hello, world'});
-  
+  @observable Map data = toObservable({'msg': 'world'});
+
   MyExample.created() : super.created();
 }
